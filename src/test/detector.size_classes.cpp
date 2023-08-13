416e83d1866743ae97f1f09389d4269e09b7118d
Nicolas van Kempen <nvankemp@gmail.com>
2023-08-12 22:05:50 -0400

    Add a basic test file

---
+++ src/test/detector.size_classes.cpp
@@ -0,0 +1,22 @@
+#include <cassert>
+#include <cstdlib>
+
+// This test is tuned for the jemalloc size classes.
+int main() {
+    void* ptr;
+
+    ptr = malloc(1);
+    ptr = realloc(ptr, 9);
+    free(ptr);
+
+    ptr = calloc(1, 16);
+    ptr = reallocarray(ptr, 1, 32);
+    free(ptr);
+
+    int result = posix_memalign(&ptr, 4096, 48);
+    assert(result == 0);
+    free(ptr);
+
+    ptr = aligned_alloc(64, 64);
+    free(ptr);
+}

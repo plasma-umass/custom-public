1888d609c95fd1543c083f5632dee43802ef7642
Nicolas van Kempen <nvankemp@gmail.com>
2023-08-28 19:19:14 -0400

    clang-format

--- src/size-classes.cpp
+++ src/size-classes.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include <iostream>
+#include <stdio.h>
 
 // To ensure correct resolution of symbols, add Psapi.lib to TARGETLIBS
 // and compile with -DPSAPI_VERSION=1
@@ -7,20 +7,20 @@
 #if !defined(PSAPI_VERSION)
 #define PSAPI_VERSION 1
 #endif
-#include <windows.h>
 #include <psapi.h>
+#include <windows.h>
 
 SIZE_T getHeapMemoryUsed() {
     SIZE_T heapSize = 0;
     DWORD heapCount = GetProcessHeaps(0, NULL);
-    
+
     if (heapCount == 0) {
         return 0;
     }
 
     HANDLE* heaps = new HANDLE[heapCount];
     heapCount = GetProcessHeaps(heapCount, heaps);
-    
+
     for (DWORD i = 0; i < heapCount; i++) {
         PROCESS_HEAP_ENTRY entry;
         entry.lpData = NULL;
@@ -37,60 +37,58 @@ SIZE_T getHeapMemoryUsed() {
             }
         }
     }
-    
+
     delete[] heaps;
     return heapSize;
 }
 
-void PrintMemoryInfo( DWORD processID )
-{
-  const auto iterations = 1'000'000;
-  auto objs = new char *[iterations];
+void PrintMemoryInfo(DWORD processID) {
+    const auto iterations = 1'000'000;
+    auto objs = new char*[iterations];
 
-  float prevOverhead = (float) INT_MAX;
-  auto prevIndex = 0;
-  auto hHeap = GetProcessHeap();
-  
-  for (auto objectSize = 0; objectSize < 256; objectSize++) {
-    auto beforeMem = getHeapMemoryUsed();
-  
-    for (int i = 0; i < iterations; i++) {
-      objs[i] = new char[objectSize];
-      for (auto j = 0; j < objectSize; j++) {
-	objs[i][j] = j % 256;
-      }
-    }
+    float prevOverhead = (float) INT_MAX;
+    auto prevIndex = 0;
+    auto hHeap = GetProcessHeap();
 
-    if (hHeap != NULL) {
-      HeapCompact(hHeap, 0);
-    }
-    
-    auto afterMem = getHeapMemoryUsed();
-    
-    auto usedMem = afterMem - beforeMem;
-    float overhead = (float) usedMem / (iterations * objectSize);
+    for (auto objectSize = 0; objectSize < 256; objectSize++) {
+        auto beforeMem = getHeapMemoryUsed();
 
-    if (overhead >= prevOverhead) {
-      std::cout << prevIndex << " (overhead = " << prevOverhead << ")" << std::endl;
-    }
-    prevOverhead = overhead;
-    prevIndex = objectSize;
-    
-    for (int i = 0; i < iterations; i++) {
-      delete [] objs[i];
-    }
+        for (int i = 0; i < iterations; i++) {
+            objs[i] = new char[objectSize];
+            for (auto j = 0; j < objectSize; j++) {
+                objs[i][j] = j % 256;
+            }
+        }
 
-    if (hHeap != NULL) {
-        HeapCompact(hHeap, 0);
+        if (hHeap != NULL) {
+            HeapCompact(hHeap, 0);
+        }
+
+        auto afterMem = getHeapMemoryUsed();
+
+        auto usedMem = afterMem - beforeMem;
+        float overhead = (float) usedMem / (iterations * objectSize);
+
+        if (overhead >= prevOverhead) {
+            std::cout << prevIndex << " (overhead = " << prevOverhead << ")" << std::endl;
+        }
+        prevOverhead = overhead;
+        prevIndex = objectSize;
+
+        for (int i = 0; i < iterations; i++) {
+            delete[] objs[i];
+        }
+
+        if (hHeap != NULL) {
+            HeapCompact(hHeap, 0);
+        }
     }
-  }
 }
 
-int main( void )
-{
-  auto pid = GetCurrentProcessId();
-  PrintMemoryInfo(pid);
-  
+int main(void) {
+    auto pid = GetCurrentProcessId();
+    PrintMemoryInfo(pid);
+
 #if 0
   // Get the list of process identifiers.
 

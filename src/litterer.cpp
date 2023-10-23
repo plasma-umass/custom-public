9079087a33505fc77de285259908cacd12fe2b88
Nicolas van Kempen <nvankemp@gmail.com>
2023-10-23 07:08:36 -0400

    FIx Windows CI

--- src/litterer.cpp
+++ src/litterer.cpp
@@ -139,7 +139,7 @@ void runLitterer() {
 
     const auto litterStart = std::chrono::high_resolution_clock::now();
 
-    std::uniform_int_distribution<std::int64_t> distribution(1, nAllocations);
+    std::uniform_int_distribution<std::uint64_t> distribution(1, nAllocations);
     std::vector<void*> objects = *(new std::vector<void*>);
     objects.reserve(nAllocationsLitter);
 
